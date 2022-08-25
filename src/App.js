0f78f068290c57646530bf29f80f24273510ed57
kumarpriyanshu2k2 <kumar.priyanshu2k02@gmail.com>
2022-08-25 13:08:41 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ReactDOM } from "react";
+import { useState } from "react";
 
 function App() {
+    const [calc, setcalc] = useState("");
+    const [result, setresult] = useState("");
+
+    const ops=['/','-','+','*','.'];
+    const updateCalc = value =>{
+      setcalc(calc+value);
+    }
+  const createDigits= ()=>{
+    var digits=[];
+    for(let i=1;i<10;i++){
+      digits.push(
+        <button onClick={()=>updateCalc(i.toString())} key={i}>{i}</button>
+      )
+    }
+
+        return digits;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+      <div className="display">
+        {result? <span>0</span>:""}
+        {calc||"0"}
+      </div>
+      <div className="operators">
+        <button onClick={()=>updateCalc('+')}>+</button>
+        <button onClick={()=>updateCalc('*')}>*</button>
+        <button onClick={()=>updateCalc('-')}>-</button>
+        <button onClick={()=>updateCalc('/')}>/</button>
+        <button>DEL</button>
+        </div>
+        <div className="digits">
+          {createDigits()}
+          <button onClick={()=>updateCalc('0')}>0</button>
+          <button onClick={()=>updateCalc('.')}>.</button>
+          <button onClick={()=>updateCalc('=')}>=</button>
+        </div>
+    </div>
     </div>
   );
 }
